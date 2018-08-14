1e12f89f6fce1cf7762af4b7cfed6ce8e81e7e7c
Caleb Smith <karagenit@outlook.com>
2018-08-14 09:12:18 -0400

    Add Labels & Legend

--- graph.r
+++ graph.r
@@ -12,12 +12,23 @@ plot(peak$Time,
      col="red",
      type="l",
      ylim=c(0, 1.3),
-     xlim=c(0, 300))
+     xlim=c(0, 300),
+     xlab="Time (s)",
+     ylab="Torque (Nm)",
+     main="Torque Output - Stalling vs Slipping")
 points(stall$Time,
        stall$Torque,
        col="blue",
        type="l")
+legend(x = "bottomright",
+       legend = c("Slipping (Peak Power)", "Stalling (6 Volts)"),
+       col = c("red", "blue"),
+       lty = 1,
+       bty = "n",
+       cex = 0.8)
+
+dev.print(png, "plot.png", width = 1000, height = 1000)
 
 Sys.sleep(1000)
 
-# Do both draw the same current? We could plot this.
+# Do both draw the same current? We could plot this, or also Torque per Amp.
